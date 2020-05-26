6986759c10f674d6436b1cec46bad666b36a8013
Kenneth Sharman <kenneth.sharman@ucalgary.ca>
2020-05-25 23:03:53 -0600

    input basics ctd

--- user_input.c
+++ user_input.c
@@ -13,6 +13,7 @@ int main()
 	char letter;
 	int integer;
 	double decimal;
+	char someString[20];
 
 	printf("Enter a letter: ");
 	scanf("%c", &letter);
@@ -22,7 +23,12 @@ int main()
 
 	printf("Enter some decimal number: ");
 	scanf("%lf", &decimal);
+
+	printf("Enter your name: ");
+	scanf("%s", someString);
+	// fgets(someString, 20, stdin) would get string w/ spaces
 	
-	printf("You entered %c, %d, %f", letter, integer, decimal);
+	printf("You entered %c, %d, %f\n", letter, integer, decimal);
+	printf("Your name is %s\n", someString);
 }
 	

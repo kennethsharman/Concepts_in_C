61f835687ad4aa2a27362c9ef783653854035e56
Kenneth Sharman <kenneth.sharman@ucalgary.ca>
2020-05-26 20:28:16 -0600

    simple calc (ctd) and simple i/o game with user

--- calculator.c
+++ calculator.c
@@ -10,15 +10,15 @@
 
 int main()
 {
-	int num1;
-	int num2;
+	double num1;
+	double num2;
 
-	printf("Enter first integer: ");
-	scanf("%d", &num1);
-	printf("Enter second integer: ");
-	scanf("%d", &num2);
+	printf("Enter first number: ");
+	scanf("%lf", &num1);
+	printf("Enter second number: ");
+	scanf("%lf", &num2);
 
-	printf("%d + %d = %d", num1, num2, num1+num2);
+	printf("%f + %f = %f", num1, num2, num1+num2);
 
 	return 0;
 }
---
+++ mad_libs.c
@@ -0,0 +1,32 @@
+/*
+ * mad_libs.c	Simple mad lib game
+ *
+ * Created on:	May 26, 2020
+ * Author:	Ken
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+	char color[20];
+	char pluralNoun[20];
+	char celebrity[20];
+
+	printf("Enter a color: ");
+	scanf("%s", color);
+
+	printf("Enter a plural noun: ");
+	scanf("%s", pluralNoun);
+
+	printf("Enter a celebrity: ");
+	scanf("%s", celebrity);
+
+	printf("Roses are %s\n", color);
+	printf("%s are blue\n", pluralNoun);
+	printf("I love %s\n", celebrity);
+
+	return 0;
+}
+

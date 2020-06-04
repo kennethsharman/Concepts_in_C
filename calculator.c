1342e8352c82c8d87488916cd8b00ce70c60e866
Kenneth Sharman <kenneth.sharman@ucalgary.ca>
2020-06-03 23:16:46 -0600

    calculator with basic operations

--- calculator.c
+++ calculator.c
@@ -1,7 +1,7 @@
 /*
  * calculator.c		Simple calculator in C
  *
- * Created on:	May 25, 2020
+ * Created on:	June 3, 2020
  * Author: 	Ken
  */
 
@@ -12,13 +12,30 @@ int main()
 {
 	double num1;
 	double num2;
+	char op;
 
 	printf("Enter first number: ");
 	scanf("%lf", &num1);
+	
 	printf("Enter second number: ");
 	scanf("%lf", &num2);
 
-	printf("%f + %f = %f", num1, num2, num1+num2);
+	printf("Enter desired operator (+,-,*, or /): ");
+	scanf(" %c", &op);	// need space in front of %c
+
+	double result;
+
+	if(op == '+') {
+		result = num1 + num2;
+	} else if(op == '-') {
+		result = num1 - num2;
+	} else if(op == '*') {
+		result = num1 * num2;
+	} else {
+		result = num1 / num2;
+	}
+
+	printf("%f %c %f = %f", num1, op, num2, result);
 
 	return 0;
 }
